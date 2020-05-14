02527865333223dfdda7f75a81dd718dd733c950
Gulyaev Stepan <nlm@yandex.ru>
2020-05-14 22:59:06 +0300

    Lesson 1

--- src/ru/geekbrains/java_2/MainCanvas.java
+++ src/ru/geekbrains/java_2/MainCanvas.java
@@ -7,12 +7,12 @@ public class MainCanvas extends JPanel {
 
     MainWindow gameController;
     long lastFrame;
-    int countBackground = 0;
-    int speedBackground = 60;
+    Background background;
 
     MainCanvas(MainWindow gameController) {
         this.gameController = gameController;
         lastFrame = System.nanoTime();
+        background = new Background();
     }
 
     @Override
@@ -22,18 +22,7 @@ public class MainCanvas extends JPanel {
         float deltaTime = (currentTime - lastFrame) * 0.000000001f;
         gameController.onDrawFrame(this, g, deltaTime);
         lastFrame = currentTime;
-        ++countBackground;
-        if (countBackground == speedBackground) {
-            setBackground(new Color((int) (Math.random() * 255),
-                    (int) (Math.random() * 255),
-                    (int) (Math.random() * 255)));
-            countBackground = 0;
-        }
-        /*
-        * Будем считать разы проходов paintComponent-а.
-        * Внимание, если поменять Thread.sleep(17);,
-        * то скорость смены фона тоже поменяется.
-        * */
+        background.setColor(this, deltaTime);
         try {
             Thread.sleep(17);
         } catch (InterruptedException e) {
