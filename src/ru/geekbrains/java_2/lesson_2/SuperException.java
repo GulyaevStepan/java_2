036423be50d4fcfc19a4f667360717491c5a0163
Gulyaev Stepan <nlm@yandex.ru>
2020-05-18 22:52:41 +0300

    Lesson 2

---
+++ src/ru/geekbrains/java_2/lesson_2/SuperException.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.java_2.lesson_2;
+
+public class SuperException extends RuntimeException {
+
+    public SuperException() {
+        super();
+    }
+
+    public SuperException (String s) {
+        super(s);
+    }
+}
